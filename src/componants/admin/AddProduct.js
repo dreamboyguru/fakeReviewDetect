c76e378731d97c4191caf7e6f547d3d715fdd184
dreamboyguru <guruhanamannavar870@gmail.com>
2024-06-21 23:47:05 +0530

    update admin-product add prodct backend

---
+++ src/componants/admin/AddProduct.js
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+const AddProduct = ({ToggleBox}) => {
+  const [load, setLoad] = useState(false);
+  
+
+  const initialValues = {
+    productName: '',
+    productImage: null
+  };
+
+  const validationSchema = Yup.object({
+    productName: Yup.string()
+      .required('Product name is required')
+      .min(3, 'Product name must be at least 3 characters long'),
+    productImage: Yup.mixed()
+      .required('Product image is required')
+  });
+
+  const handleSubmit = (values, { setSubmitting, resetForm }) => {
+    const formData = new FormData();
+    formData.append('productName', values.productName);
+    formData.append('productImage', values.productImage);
+
+    setLoad(true);
+
+    axios.post('http://localhost:5000/api/products', formData)
+      .then(response => {
+        console.log(response.data);
+        window.location.reload();
+      })
+      .catch(error => {
+        console.error(error);
+        setLoad(false);
+        setSubmitting(false);
+      });
+  };
+
+  return (
+    <div className='fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-50 '>
+      <div className='relative w-2/3 max-w-md h-auto bg-white rounded-md shadow-lg p-6'>
+        <div className='absolute top-2 right-4 hover:scale-110' onClick={ToggleBox}>X</div>
+        <div className='mb-4 text-center'>
+          <h2 className='text-2xl font-semibold'>Add New Product</h2>
+        </div>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ setFieldValue, isSubmitting }) => (
+            <Form className='space-y-4'>
+              <div>
+                <label className='block text-sm font-medium text-gray-700'>Enter name of product</label>
+                <Field
+                  name='productName'
+                  type='text'
+                  className='mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+                />
+                <ErrorMessage
+                  name='productName'
+                  component='div'
+                  className='text-red-600 text-sm'
+                />
+              </div>
+              <div>
+                <label className='block text-sm font-medium text-gray-700'>Add image here</label>
+                <input
+                  name='productImage'
+                  type='file'
+                  className='mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+                  onChange={(event) => {
+                    setFieldValue('productImage', event.currentTarget.files[0]);
+                  }}
+                />
+                <ErrorMessage
+                  name='productImage'
+                  component='div'
+                  className='text-red-600 text-sm'
+                />
+              </div>
+              <div>
+                {load ? (
+                  <button
+                    type='submit'
+                    disabled
+                    className='bg-gray-400 text-white p-2 w-full rounded-md'
+                  >
+                    Submitting...
+                  </button>
+                ) : (
+                  <button
+                    type='submit'
+                    disabled={isSubmitting}
+                    className='bg-gray-600 hover:bg-gray-800 text-white p-2 w-full rounded-md hover:scale-105 transform duration-500'
+                  >
+                    Submit
+                  </button>
+                )}
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+};
+
+export default AddProduct;
--- src/componants/admin/Products.js
+++ src/componants/admin/Products.js
@@ -1,9 +1,88 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AddProduct from './AddProduct';
+import axios from 'axios';
 
 const Products = () => {
+    const url = process.env.REACT_APP_API_URL
+    const [data, setData] = useState(false);
+    const [box, setBox] = useState(false);
+
+    useEffect(()=> {
+        const fetchData = async() => {
+            try {
+                const response = await axios.get(`${url}/api/products`);
+                console.log(response.data);
+                setData(response.data)
+            } catch (err) {
+                console.log(err);
+                setData([
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '4.7' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                    { id: 1, name: 'Heaven', image: 'https://www.tamilnadutourism.tn.gov.in/img/pages/vertical/tiger-hill-1680516074_a0a74c97dfcd912c936f.webp', rate: '*****' },
+                ])
+            }
+        }
+        fetchData();
+      }, []);
+     
+    const ToggleBox = () =>{
+        setBox(!box)
+    }
   return (
-    <div className='bg-gray-500 w-96 h-screen'>
-      
+    <div className={`bg-gray-100 w-full h-screen`}>
+        {box && <AddProduct ToggleBox={ToggleBox}/> }
+        <button 
+            className={`bg-gray-600 rounded-md hover:bg-slate-800 text-white p-2 px-5 mt-32 hover:scale-105 duration-500 ${box ? 'blur -mt-20 z-20' : '' } `}
+            onClick={()=>ToggleBox()}            
+        >Add New</button>
+
+
+        <section className={`p-10 max-md:p-0 max-md:pt-24 ${box ? 'blur -mt-20 z-20' : '' }`}>
+            <div className="rounded-lg p-1 ">
+                <div className="flex flex-wrap gap-3 justify-between">
+                    {data && data.map(item => (
+                        <div className="w-56 max-md:w-48 max-sm:w-[48%]  bg-white  p-2 max-sm:p-2 h-auto rounded-lg shadow-lg" key={item.id}>
+                            <div className="rounded overflow-hidden mb-2">
+                                <img 
+                                    src={`${url}/uploads/item.image`} 
+                                    alt={item.name}
+                                    className="h-44 max-sm:h-36 w-full object-cover"
+                                />
+                            </div>
+                            <div>
+                                <label className="block text-lg font-bold mb-1">{item.name}</label>
+                                <div className='flex flex-row justify-between items-center'>
+                                    {/* <div className="text-sm text-gray-600"><span className='text-yellow-600 text-2xl'>{item.ra}</span></div> */}
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+
     </div>
   )
 }
